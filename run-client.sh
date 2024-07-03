07f18d553f6b9b8907f22fe3876309fe7c3b4eba
arshiaakhavan <letmemakenewone@gmail.com>
2024-07-03 19:08:24 +0330

    Better support for ip route in run-client

--- run-client.sh
+++ run-client.sh
@@ -12,7 +12,10 @@ trap 'kill $pid' INT TERM
 sleep 1
 
 # replace with server IP
-sudo ip route `ip route | grep default | sed 's/default/87.247.189.1/g'`
+# sudo ip route add `ip route | grep default | sed 's/default/87.247.189.1/g'`
+while IFS= read -r line; do
+    sudo ip route add $line
+done <<< `ip route | grep default | sed 's/default/87.247.189.1/g'`
 sudo ip route add default dev ark-0
 
 echo "route neverssl.com"
