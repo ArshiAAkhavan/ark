f47bbec326c6351893c7d551ff287ad3163647d8
arshiaakhavan <letmemakenewone@gmail.com>
2024-07-26 15:21:45 +0330

    enable ipv4_forward in server

--- run-server.sh
+++ run-server.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 cargo build --release
 sudo setcap cap_net_admin=eip ./target/release/ark
@@ -10,6 +10,7 @@ pid=$!
 trap "kill $pid" INT TERM
 
 sleep 1
+sudo sysctl -w net.ipv4.ip_forward=1
 iptables -t nat -A POSTROUTING -s 172.16.0.0/24 ! -d 172.16.0.0/24 -j MASQUERADE
 echo "nat is done"
 
